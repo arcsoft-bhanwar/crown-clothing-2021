8295b96c8e4ee77486ec540d4694fdbbfc03bb37
Bhanwar Singh Rathore <rathore@Bhanwars-iMac.local>
2021-07-31 13:43:23 +0530

    code pushed for collection item

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import{Route, Switch} from 'react-router-dom'
-import ShopPage from './pages/shop/shop.component';
-
-import HomePage from './pages/homepage/homepage.component'
 import './App.scss';
-
+import ShopPage from './pages/shop/shop.component';
+import HomePage from './pages/homepage/homepage.component'
+import Header from './components/header/headre.component'
 
 const App =()=> {
   return (
     <div>
+      <Header/>
       <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path ='/shop' component={ShopPage}/>
